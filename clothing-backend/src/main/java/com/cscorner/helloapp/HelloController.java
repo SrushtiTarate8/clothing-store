02d9d3918b9cc903d8ae8db7955f1ef3cfac088a
SrushtiTarate8 <srushtitarate83@gmail.com>
2026-04-04 00:33:03 +0530

    Initial commit - clothing store project

---
+++ clothing-backend/src/main/java/com/cscorner/helloapp/HelloController.java
@@ -0,0 +1,12 @@
+package com.cscorner.helloapp;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController{
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello from CS Corner!!";
+    }
+}
